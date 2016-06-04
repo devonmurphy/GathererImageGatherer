7eeee5d5f4b73dad78d6e2e014a1771fae13d4e7
Devon Murphy <devonmurphy1@gmail.com>
2016-06-04 01:57:51 -0700

    Currently this downloads all the card images on the page

--- scrapeImage.py
+++ scrapeImage.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from urllib2 import urlopen
+import urllib
 import requests
 
 url="http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&set=%5B\"Worldwake\"%5D"
@@ -7,8 +7,10 @@ url="http://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&set=%5B\"World
 result = requests.get(url)
 html = result.content
 soup = BeautifulSoup(html,"lxml")
-samples = soup.find_all("div","cardInfo")
-
-print(samples[0])
-
-
+samples = soup.find_all("span","cardTitle")
+id =[]
+for i in range(0,len(samples)):
+	hold= samples[i].a.attrs['href']
+	id.append(hold[34:])
+ 	URL = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%s&type=card" %hold[34:]
+	urllib.urlretrieve(URL, "cardImages/%s.jpg" %hold[34:])
