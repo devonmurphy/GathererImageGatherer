c5f192a15dd664e8b102e73380062ba953bd36a8
Devon Murphy <devonmurphy1@gmail.com>
2016-06-03 22:14:35 -0700

    Fixed cardSets.txt

--- scrapeImage.py
+++ scrapeImage.py
@@ -1,5 +1,25 @@
 import urllib
+newSets = open('newSets.txt','r+')
 
+
+with open('cardSets.txt') as sets:
+	for line in sets:
+		buffer =""
+		for letter in range(0,len(line)):
+			if(line[letter] == '"'):
+				letter = len(line)
+			elif(line[letter]=="\n"):
+				letter = len(line)+1
+			else:
+				buffer+=line[letter]
+			if(letter == len(line)):
+				newSets.write(buffer+"\n")
+				
+newSets.close
+sets.close
+"""
 for x in range(1,410064):
     URL = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=%d&type=card" %x
     urllib.urlretrieve(URL, "cardImages/%d.jpg" %x)
+"""
+
