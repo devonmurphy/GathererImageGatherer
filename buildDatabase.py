3d9aa62448cbbe29b9a6c05afe5254fe9b4f723c
Devon Murphy <devonmurphy1@gmail.com>
2016-06-05 22:24:48 -0700

    Added program to build data base of perceptual hashes of cards

---
+++ buildDatabase.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+import os
+import sys
+import imagehash
+import numpy
+from PIL import Image
+import psycopg2
+
+
+def getHash(img):
+	normal = Image.open(img).convert('L')
+	crop=normal.crop((17,37,205,150))
+	hash = str(imagehash.average_hash(normal))
+	return hash
+
+def addToDb(con,cur,name,set,hash):
+	command = "INSERT INTO phash (name,set,hash) VALUES('"+name+"','"+set+"','"+hash+"');"
+	cur.execute(command)          
+	con.commit()
+
+def getCardInfo(card):
+	card =card[11:]
+	card = card.replace('%20',' ')
+	card = card.replace("'","%")
+	n=0
+	cardName =''
+	setName =''
+	for letter in card:
+		if(letter =='-'):
+			cardName=card[:n]
+			setName=card[n+1:-4]
+			break
+		n+=1
+	return (cardName,setName)
+yourpath = 'cardImages/'
+con = psycopg2.connect(database='cardimages', user='Devon')
+cur = con.cursor()
+
+for root, dirs, files in os.walk(yourpath, topdown=False):
+    for name in files:
+	hold =os.path.join(root, name)
+	addToDb(con,cur,getCardInfo(hold)[0],getCardInfo(hold)[1],getHash(hold))
