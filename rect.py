afaf4ea2cf2ed201120c4678c67ef3e1dd9f2da6
Devon Murphy <devonmurphy1@gmail.com>
2016-06-22 23:37:16 -0700

    Cleaned up rotate math with built in functions and fixed the cropping

--- rect.py
+++ rect.py
@@ -1,8 +1,27 @@
 #from http://www.pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
+#https://codeplasma.com/2012/12/03/getting-webcam-images-with-python-and-opencv-2-for-real-this-time/
 import cv2
+import cv
 import numpy as np
 import math
+
+camera_port =0 
+ramp_frames = 30
+"""
+camera = cv2.VideoCapture(camera_port)
+
+def get_image():
+    retval, im = camera.read()
+    return im
+
+for i in xrange(ramp_frames):
+    temp = get_image()
+camera_capture = get_image()
+file = "frame.jpg"
+cv2.imwrite(file,camera_capture)
+"""
 img = cv2.imread('kessig.jpg')
+
 orig = img.copy()
 img = cv2.resize(img,(0,0),fx=.25,fy=.25)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -11,12 +30,6 @@ edged = cv2.Canny(gray, 30, 200)
 (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
 screenCnt = None
-
-
-def destroy_brackets(list1):
-	    return str(list1).replace('[','').replace(']','')
-
-
 
 # loop over our contours
 for c in cnts:
@@ -29,21 +42,18 @@ for c in cnts:
 	if len(approx) == 4:
 		screenCnt = approx
 		break
-cv2.drawContours(img, [screenCnt], -1, (0, 255, 0), 3)
-s=screenCnt
-direct = s[0][0]-s[1][0]
-mag_direc=np.linalg.norm(direct)
-theta = math.acos((s[0][0][0]-s[2][0][0])/mag_direc)
-(h,w) = img.shape[:2]
-xes=( s[0][0][0],s[1][0][0],s[2][0][0],s[3][0][0])
-ys = (s[0][0][1],s[0][0][1],s[2][0][1],s[3][0][1])
-mid = (np.average(xes),np.average(ys))
-w=math.fabs(xes[0]-xes[3])
-h=math.fabs(ys[0]-ys[3])
-theta =math.atan(h/w)*180/math.pi+180
-print theta
+
+mask = np.zeros_like(img) # Create mask where white is what we want, black otherwise
+cv2.drawContours(mask, [screenCnt], -1, (255,255,255), -1)
+out = np.zeros_like(img)
+out[mask == 255] = img[mask == 255]
+
+rect = cv2.minAreaRect(screenCnt)
+theta = rect[2]-90
+mid = rect[0]
+
 M = cv2.getRotationMatrix2D(mid, theta, 1.0)
-rotated = cv2.warpAffine(img, M, img.shape[:2])
+rotated = cv2.warpAffine(out, M, (2*img.shape[0],2*img.shape[1]))
+crop = rotated[(int)(mid[1]-rect[1][0]/2):(int)(mid[1]+rect[1][0]/2),(int)(mid[0]-rect[1][1]/2):(int)(mid[0]+rect[1][1]/2)]
+cv2.imwrite("crop.jpg",crop)
 cv2.imwrite("rotated.jpg",rotated)
-crop = rotated[min(xes):max(xes),min(ys):max(ys)]
-cv2.imwrite('crop.jpg',crop)
