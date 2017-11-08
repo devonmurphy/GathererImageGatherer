92edcd3279d8a31c019ad8371c696f3a09f89efa
Devon Murphy <devon.murphy@fuseintegration.com>
2017-11-07 23:47:56 -0800

    Update build database to use bytea data type, added sql to create the hashes table

--- buildDatabase.py
+++ buildDatabase.py
@@ -9,34 +9,44 @@ import psycopg2
 
 def getHash(img):
 	normal = Image.open(img).convert('L')
-	crop=normal.crop((17,37,205,150))
-	hash = str(imagehash.average_hash(crop))
-	return hash
+	crop=normal.crop((25,37,195,150))
+	ahash = str(imagehash.average_hash(crop))
+        phash = str(imagehash.phash(crop))
+	psimplehash = str(imagehash.phash_simple(crop))
+	dhash = str(imagehash.dhash(crop))
+	vertdhash = str(imagehash.dhash_vertical(crop))
+	whash = str(imagehash.whash(crop))
+	return ahash,phash,psimplehash,phash,vertdhash,whash
 
-def addToDb(con,cur,name,set,hash):
-	command = "INSERT INTO phash (name,set,hash) VALUES('"+name+"','"+set+"','"+hash+"');"
+def addToDb(name,set,hashes):
+        print hashes,"---",name,"---",set
+	command = "INSERT INTO hashes (name,set,ahash,phash,psimplehash,dhash,vertdhash,whash) VALUES('"
+        command +=name+"','"+set+"'"
+        for i in range (0,6):
+            command+=",decode('"+hashes[i]+"','hex')"
+        command+=");"
 	cur.execute(command)          
 	con.commit()
 
 def getCardInfo(card):
 	card =card[11:]
 	card = card.replace('%20',' ')
-	card = card.replace("'","%")
+	card = card.replace("'","''")
 	n=0
 	cardName =''
 	setName =''
-	for letter in card:
-		if(letter =='-'):
-			cardName=card[:n]
-			setName=card[n+1:-4]
-			break
-		n+=1
+        cardDetails = card.split('   ')
+	cardName=cardDetails[0]
+        setName=cardDetails[1][:-4]
 	return (cardName,setName)
-yourpath = 'cardImages/'
+
 con = psycopg2.connect(database='cardimages', user='Devon')
 cur = con.cursor()
+cur.execute("delete from hashes")
+con.commit()
 
-for root, dirs, files in os.walk(yourpath, topdown=False):
+for root, dirs, files in os.walk('cardImages/', topdown=False):
     for name in files:
 	hold =os.path.join(root, name)
-	addToDb(con,cur,getCardInfo(hold)[0],getCardInfo(hold)[1],getHash(hold))
+        if(name!="___images-go-here.txt"):
+	    addToDb(getCardInfo(hold)[0],getCardInfo(hold)[1],getHash(hold))
---
+++ createTable.sql
@@ -0,0 +1,9 @@
+create table hashes(
+name text,
+set text,
+ahash bytea,
+phash bytea,
+psimplehash bytea,
+dhash bytea,
+vertdhash bytea,
+whash bytea);
